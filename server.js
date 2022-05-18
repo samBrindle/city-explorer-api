9115279fec2186d7e79b8bb45581598277898217
samBrindle <brindlejsam@gmail.com>
2022-05-17 18:21:51 -0700

    added weather functionality

--- server.js
+++ server.js
@@ -23,10 +23,29 @@ app.get('/', (req, res) => {
     res.send('hellooo');
 });
 
+// /hello?name=sam  **present for future me :)
 app.get('/hello', (req,res) => {
     console.log(req.query.name);
     res.send(`Hello! ${req.query.name}`);
 });
+
+app.get('/weather', (req,res) => {
+    // query parameters here?
+    try {
+        let selectedCity = data.find( (location) => 
+            location.city_name === req.query.city_name);
+
+        // res.send(`${selectedCity.city_name}`);
+        let dataToSend = selectedCity.data.map( (day) => {
+            return new Forecast(day);
+        });
+        console.log(dataToSend);
+        res.send(dataToSend);
+    } catch (error) {
+        // add error catch later
+        console.log(error);
+    }
+})
 
 // catch all "star route"
 app.get('*', (req, res) => {
@@ -34,6 +53,14 @@ app.get('*', (req, res) => {
 })
 
 // ERRORS
+
+// CLASSES 
+class Forecast {
+    constructor(dayObject) {
+        this.date = dayObject.valid_date;
+        this.description = dayObject.weather.description;
+    }
+}
 
 // LISTEN
 // start the server
