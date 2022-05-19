7ea757b5633a35df6be12d71e7913233bf311072
samBrindle <brindlejsam@gmail.com>
2022-05-18 18:27:42 -0700

    got weather api working

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 'use strict';
 
-console.log('Our first server');
-
+// REQUIRE
+// things we need to build a server
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 // const { response } = require('express');
 const axios = require('axios');
-// REQUIRE
-// things we need to build a server
 
-
-let data = require('./data/weather.json');
-
+// let data = require('./data/weather.json');
 
 // USE
 const app = express();
-
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
@@ -34,23 +29,43 @@ app.get('/hello', (req,res) => {
     res.send(`Hello! ${req.query.name}`);
 });
 
-app.get('/weather', (req,res) => {
+app.get('/weather', async (req,res, next) => {
     // query parameters here?
     try {
-        let selectedCity = data.find( (location) => location.city_name === req.query.city_name)
+        let searchLon = req.query.lon;
+        let searchLat = req.query.lat;
 
+        let url =  `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${searchLat}&lon=${searchLon}&key=${process.env.WEATHER_API_KEY}&units=I&days=5`
 
-
-        let dataToSend = selectedCity.data.map( (day) => {
+        let results = await axios.get(url);
+        console.log(results.data.data[0]);
+        let dataToSend = results.data.data.map( day => {
             return new Forecast(day);
         });
+
+        console.log(dataToSend);
         res.send(dataToSend);
+
+        // let selectedCity = data.find( (location) => location.city_name === req.query.city_name)
+
+        // let dataToSend = selectedCity.data.map( (day) => {
+        //     return new Forecast(day);
+        // });
+        // res.send(dataToSend);
     } catch (error) {
         // add error catch later
         next(error);
         console.log(error);
     }
 })
+
+app.get('/movies', async (req,res,next) => {
+    try {
+        let searchCity = req.query.city_name;
+
+        let url 
+    }
+});
 
 // catch all "star route"
 app.get('*', (req, res) => {
@@ -60,7 +75,7 @@ app.get('*', (req, res) => {
 // ERRORS
 app.use((error, req, res, next) => {
     console.log(error.message)
-    response.status(500).send(error.message)
+    res.status(500).send(error.message)
 });
 
 // CLASSES 
@@ -68,8 +83,24 @@ class Forecast {
     constructor(dayObject) {
         this.date = dayObject.valid_date;
         this.description = dayObject.weather.description;
+        this.low_temp = dayObject.low_temp;
+        this.high_temp = dayObject.high_temp;
     }
 }
+
+/*
+class Movie {
+    constructor(movieObject???) {
+        this.title = movieObject.original_title;
+        this.overview = movieObject.overview;
+        this.avg_votes = movieObject.vote_average;
+        this.vote_count = movieObject.vote_count;
+        this.img_url = movieObject.poster_path;
+        this.popularity = movieObject.popularity;
+        this.released_on = movieObject.release_date;
+    }
+}
+*/
 
 // LISTEN
 // start the server
