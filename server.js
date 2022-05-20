0b513134e017c40a852cc95e155df7508973e516
samBrindle <brindlejsam@gmail.com>
2022-05-20 11:24:50 -0700

    modulized

--- server.js
+++ server.js
@@ -5,8 +5,9 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
-// const { response } = require('express');
 const axios = require('axios');
+const getMovies = require('./modules/movies');
+const getWeather = require('./modules/weather');
 
 // let data = require('./data/weather.json');
 
@@ -29,48 +30,9 @@ app.get('/hello', (req,res) => {
     res.send(`Hello! ${req.query.name}`);
 });
 
-app.get('/weather', async (req,res, next) => {
-    // query parameters here?
-    try {
-        let searchLon = req.query.lon;
-        let searchLat = req.query.lat;
+app.get('/weather', getWeather);
 
-        let url =  `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${searchLat}&lon=${searchLon}&key=${process.env.WEATHER_API_KEY}&units=I&days=5`
-
-        let results = await axios.get(url);
-        console.log(results.data.data[0]);
-        let dataToSend = results.data.data.map( day => {
-            return new Forecast(day);
-        });
-
-        console.log(dataToSend);
-        res.send(dataToSend);
-    } catch (error) {
-        // add error catch later
-        next(error);
-        console.log(error);
-    }
-})
-
-app.get('/movies', async (req,res,next) => {
-    try {
-        let searchCity = req.query.city_name;
-
-        let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchCity}`
-
-        let results = await axios.get(url);
-
-        let dataToSend = results.data.results.map( movie => {
-            return new Movie(movie);
-        });
-
-        console.log(results.data.results);
-        res.send(dataToSend);
-    } catch (error) {
-        next(error);
-        console.log(error);
-    }
-});
+app.get('/movies', getMovies);
 
 // catch all "star route"
 app.get('*', (req, res) => {
@@ -84,26 +46,7 @@ app.use((error, req, res, next) => {
 });
 
 // CLASSES 
-class Forecast {
-    constructor(dayObject) {
-        this.date = dayObject.valid_date;
-        this.description = dayObject.weather.description;
-        this.low_temp = dayObject.low_temp;
-        this.high_temp = dayObject.high_temp;
-    }
-}
-
-class Movie {
-    constructor(movieObject) {
-        this.title = movieObject.original_title;
-        this.overview = movieObject.overview;
-        this.avg_votes = movieObject.vote_average;
-        this.vote_count = movieObject.vote_count;
-        this.img_url = "https://image.tmdb.org/t/p/w500"  + movieObject.poster_path;
-        this.popularity = movieObject.popularity;
-        this.released_on = movieObject.release_date;
-    }
-}
+// Inside modules
 
 // LISTEN
 // start the server
